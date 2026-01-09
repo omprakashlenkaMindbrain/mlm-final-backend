7f732cb66c7766808c2208fdd7e2f317d198f4e5
omprakash447 <ommprakashlenka958@gmail.com>
2026-01-10 00:44:17 +0530

    final backend

--- src/Admin/controller/admin.postpayout.controller.ts
+++ src/Admin/controller/admin.postpayout.controller.ts
@@ -3,8 +3,11 @@ import autocollectionmodel from "../../autocollection/models/autocollection";
 import UserModel from "../../User/models/user.model";
 import { PayoutHistoryModel } from "../models/payouthistory.model";
 import { payout } from "../services/payout.service";
+import kycmodel from "../../kyc/models/kyc.models";
 
-export async function postPayoutController(_req:Request,res: Response) {
+
+
+export async function postPayoutController(_req: Request, res: Response) {
   try {
     const autoCollection = await autocollectionmodel.findOne().lean();
 
@@ -26,25 +29,32 @@ export async function postPayoutController(_req:Request,res: Response) {
       });
     }
 
-  var responseData: any[] = [];
+    const responseData: any[] = [];
 
     for (const item of payoutUsers) {
       const user = await UserModel.findById(item.userId);
       if (!user) continue;
 
       const recentIncome = Number(user.recentIncome);
-      if (recentIncome <= 0) continue;
+      // if (recentIncome <= 0) continue;
 
       const totalIncome = Number(user.totalIncome || 0);
       const totalwithdrawincome = Number(user.totalwithdrawincome || 0);
 
-      
-      const adminAmount =autoCollection.admincharges;
+      const kycDetails = await kycmodel
+        .findOne({
+          userid: user._id.toString(),
+          status: "approved",
+          isActive: true,
+        })
+        .lean();
+
+      const adminAmount = autoCollection.admincharges;
       const tdsAmount =
         (recentIncome * autoCollection.tds) / 100;
       const netPayout = recentIncome - (adminAmount + tdsAmount);
 
-      
+
       const payoutHistory = await PayoutHistoryModel.create({
         userId: user._id,
         userRecentIncome: recentIncome,
@@ -55,22 +65,23 @@ export async function postPayoutController(_req:Request,res: Response) {
         status: "success",
       });
 
-      
+
       user.totalwithdrawincome = totalwithdrawincome + recentIncome;
-      user.netincome = totalIncome-user.totalwithdrawincome;
+      user.netincome = totalIncome - user.totalwithdrawincome;
       user.recentIncome = 0;
 
       const savedUser = await user.save();
       responseData.push({
-        user: {
-          _id: savedUser._id,
-          email: savedUser.email,
-          name: savedUser.name,
-          totalwithdrawincome: savedUser.totalwithdrawincome,
-           
-          netincome: savedUser.netincome,
+        // user: {
+        //   _id: savedUser._id,
+        //   email: savedUser.email,
+        //   name: savedUser.name,
+        //   totalwithdrawincome: savedUser.totalwithdrawincome,
 
-        },
+        //   netincome: savedUser.netincome,
+        // },
+        user: savedUser,
+        kyc: kycDetails,
         payoutHistory: {
           payoutId: payoutHistory._id,
           grossIncome: recentIncome,
@@ -78,12 +89,32 @@ export async function postPayoutController(_req:Request,res: Response) {
           tds: tdsAmount,
           netPayout: netPayout,
           date: payoutHistory.createdAt,
-        }
-        
+        },
       });
-      
+      //   user: {
+      //     _id: savedUser._id,
+      //     email: savedUser.email,
+      //     name: savedUser.name,
+      //     totalwithdrawincome: savedUser.totalwithdrawincome,
+
+      //     netincome: savedUser.netincome,
+
+      //   },
+      //   kyc: kycDetails,
+      //   payoutHistory: {
+      //     payoutId: payoutHistory._id,
+      //     grossIncome: recentIncome,
+      //     adminCharges: adminAmount,
+      //     tds: tdsAmount,
+      //     netPayout: netPayout,
+      //     date: payoutHistory.createdAt,
+      //   }
+
+      // });
+      console.log(responseData);
+
     }
-    
+
     return res.status(200).json({
       success: true,
       message: "Payout processed successfully",
